dcd5ead898b5654866ba1756a591c8242735c7fe
Katre <tikukatre@gmail.com>
2019-11-30 23:48:55 +0200

    Veebilehe versioon uue vastu vahetatud

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 /* eslint-env browser */
 /*  js kood lehelt https://www.w3schools.com/howto/howto_js_collapsible.asp */
-var coll = document.getElementsByClassName("collapsible");
-var i;
-for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function () {
-        this.classList.toggle("active"); 
-        var content = this.nextElementSibling; 
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null; 
-        } else {
-            content.style.maxHeight = content.scrollHeight + "px"; 
-        } 
-    }); 
-} 
+window.addEventListener("click", function(e) {
+if (e.target.classList.contains("tab-button")) {
+var href = e.target.getAttribute("href");
+var hash = href.substring(1, href.length);
+document.getElementById(hash).classList.toggle("is-active");
+e.preventDefault();
+}else {
+                var parentClass = e.target.parentNode.classList;
+                if (!parentClass.contains("image-info") && !parentClass.contains("image-info-container")) {
+                    var imageInfoBoxes = document.getElementsByClassName("image-info");
+                    for (var i = 0; i < imageInfoBoxes.length; i++) {
+                        imageInfoBoxes[i].classList.remove("is-active");
+                    }
+                }
+            }
+});
