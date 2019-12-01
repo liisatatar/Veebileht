0a9c201305462b25a15c214361507f61e053673f
kristjansims <kristjansims@gmail.com>
2019-12-01 13:35:05 +0200

    Syntax fixes

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ var href = e.target.getAttribute("href");
 var hash = href.substring(1, href.length);
 document.getElementById(hash).classList.toggle("is-active");
 e.preventDefault();
-}else {
-                var parentClass = e.target.parentNode.classList;
-                if (!parentClass.contains("image-info") && !parentClass.contains("image-info-container")) {
-                    var imageInfoBoxes = document.getElementsByClassName("image-info");
-                    for (var i = 0; i < imageInfoBoxes.length; i++) {
-                        imageInfoBoxes[i].classList.remove("is-active");
-                    }
-                }
+    } else{
+        var parentClass = e.target.parentNode.classList;
+        if (!parentClass.contains("image-info") && !parentClass.contains("image-info-container")) {
+            var imageInfoBoxes = document.getElementsByClassName("image-info");
+            for (var i = 0; i < imageInfoBoxes.length; i++) {
+                imageInfoBoxes[i].classList.remove("is-active");
             }
+        }
+    }
 });
